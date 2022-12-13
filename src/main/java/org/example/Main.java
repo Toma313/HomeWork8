a57bd4d5f9762f2e6a65dbe40a8609871f01ac61
tamara.voloshchuk <tamara.voloshchuk01@gmail.com>
2022-12-14 00:48:09 +0200

    newHome work 8

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.List;
+
+import static org.example.ThisMethod.myMethod;
+
+public class Main {
+    public static void main(String[] args) {
+        myMethod("butter,  butter,  sausage,  milk");
+
+
+        }
+    }
---
+++ src/main/java/org/example/ThisMethod.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThisMethod {
+    private static String str;
+
+    public static void myMethod(String str) {
+        String[] massive = str.split(", .");
+        Arrays newArray = Arrays.stream(massive).toArray();
+        ArrayList<Integer> integerArrayList = newArray.asList(massive);
+        for (String s : massive) {
+            System.out.println(s.substring(0,1).toUpperCase()+s.substring(1));
+
+
+        }
+    }
+
+}
